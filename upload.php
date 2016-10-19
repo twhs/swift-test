68d421259862b2e46b51713f55fa56b20d5eb153
twhs <twhs768@gmail.com>
2016-10-20 00:20:14 +0900

    initial commit

---
+++ upload.php
@@ -0,0 +1,30 @@
+<?php
+
+require "/var/www/html/download/vendor/autoload.php";
+
+use OpenCloud\OpenStack;
+use Guzzle\Http\Exception\ClientErrorResponseException;
+
+$opts = [
+    'username' => '',
+    'password' => '',
+    'tenantName' => ''
+];
+
+$auth_url = 'https://identity.tyo1.conoha.io/v2.0';
+$container_name = 'test';
+$file_name = '50MB';
+
+try {
+    $client = new OpenStack($auth_url, $opts);
+    $client->authenticate();
+
+    $service = $client->objectStoreService('Object Storage Service', 'tyo1');
+
+    $container = $service->getContainer($container_name);
+    $container->uploadObject($file_name, fopen('./dummy100', 'r+'));
+} catch (ClientErrorResponseException $e) {
+    echo $e->getMessage();
+}
+
+echo 'success';
